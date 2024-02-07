e26bae9f8118b7fef8074df763d6f03f10d52d47
evlakho <158267311+kayooq@users.noreply.github.com>
2024-02-07 11:29:01 +0300

    Create z6.py

---
+++ z6.py
@@ -0,0 +1,8 @@
+import math
+k = int(input("введите к "))
+x = int(input("введите х "))
+y = int(input("введите у "))
+print("1)", 100**3-92**2+k)
+print("2)", math.sqrt((1-math.cos(x))/2))
+print("3)", (math.sqrt(abs(x)) * math.log(x**2)) / (-5 / 4*x + math.exp(x/2)))
+print("4)", math.sqrt(math.sin(x)**2 + math.cos(y**3)**2))
