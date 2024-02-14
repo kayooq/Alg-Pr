dfff9773870ca787553624cdcdd4e9978cdaf125
evlakho <158267311+kayooq@users.noreply.github.com>
2024-02-14 14:09:51 +0300

    Update Pr-2.2.py

--- Pr-2.2.py
+++ Pr-2.2.py
@@ -2,12 +2,11 @@ x = int(input())
 y = int(input())
 if x >= y:
     max = x
+    min = y
 else:
     max = y
-if x <= y:
     min = x
-else:
-    min = y
+
 print("1) z =", (min + 0.5)/(1+max**2))
 if x >= 0:
     print("2) z =",max)
@@ -17,7 +16,7 @@ if y >= 0:
     print("3) z =",min)
 else:
     if x**2 >= y**2:
-        max = x
+        max = x**2
     else:
-        min = y
+        min = y**2
     print("3) z =", max)
