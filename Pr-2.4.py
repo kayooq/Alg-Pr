f042c5e51723fabf33312b1460447ea1fb87004b
evlakho <158267311+kayooq@users.noreply.github.com>
2024-02-14 14:06:38 +0300

    Update Pr-2.4.py

--- Pr-2.4.py
+++ Pr-2.4.py
@@ -1,4 +1,14 @@
 a = int(input())
 b = int(input())
 c = int(input())
-print("P =", (max(a,b,c)+min(a,b,c))/2)
+maxABC = a
+minABC = a
+if b > maxABC:
+    maxABC = b
+if c > maxABC:
+    maxABC = c
+if b < minABC:
+    minABC = b
+if c < minABC:
+    minABC = c
+print("P =", (maxABC+minABC)/2)
