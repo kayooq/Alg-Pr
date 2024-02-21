1fd1680502851807b4efdf6c2801646aa4cfd378
evlakho <158267311+kayooq@users.noreply.github.com>
2024-02-21 11:24:08 +0300

    Create Pr-2.11.py

---
+++ Pr-2.11.py
@@ -0,0 +1,13 @@
+x, y = map(float, input().split())
+if x**2+y**2<=1:
+    print("а) принадлежит")
+else:
+    print("a) не принадлежит")
+if x**2+y**2<=1 and 0.5<=x<=1 and 0.5<=y<=1:
+    print("б) принадлежит")
+else:
+    print("б) не принадлежит")
+if x<=1 and y<=1:
+    print("в) принадлежит")
+else:
+    print("в) не принадлежит")
