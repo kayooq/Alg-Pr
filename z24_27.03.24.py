b3c34fa3f3059835549a15b1576ac01e0724d5b2
evlakho <158267311+kayooq@users.noreply.github.com>
2024-03-27 14:13:47 +0300

    Update z24_27.03.24.py

--- z24_27.03.24.py
+++ z24_27.03.24.py
@@ -1,4 +1,4 @@
-from math import sqrt
+from math import sqrt,factorial as fack
 e = int(input())
 n = 1 
 a = [0]
@@ -8,4 +8,16 @@ a.append(n/(sqrt((n**2)+1)-sqrt((n**2)-1)))
 while abs(a[n]-a[n-1])<e:
     n+=1
     a.append(n/(sqrt((n**2)+1)-sqrt((n**2)-1)))
-print(a[n])
+print('1)',a[n])
+a = [0,0.5,0.5*2/3]
+n = 2
+while abs(a[n]-a[n-1])>e:
+    n+=1
+    a.append(a[n-1]*(1-(1/n)))
+print('2)',a[n])
+a = [0,1-1/fack(2),1+1/fack(3)]
+n = 2
+while abs(a[n]-a[n-1])>e:
+    n+=1
+    a.append(a[n-1]*(1+(-1**n-1)/(fack(n))))
+print('3)',a[n])
